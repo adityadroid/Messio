1b8556df3b48bf5897e7f40d5019097bdfa3527e
Aditya Gurjar <aditya.gurjar@savaari.com>
2019-08-14 00:20:32 +0530

    Added hello world test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,8 +4,6 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:messio/main.dart';
