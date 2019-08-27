2f2edcabb55c23901c64c7594883ae744212b36d
Aditya Gurjar <aditya.gurjar@savaari.com>
2019-08-28 01:02:14 +0530

    Removed redundant keyword

--- lib/pages/ConversationPageSlide.dart
+++ lib/pages/ConversationPageSlide.dart
@@ -14,7 +14,7 @@ class ConversationPageSlide extends StatefulWidget {
 class _ConversationPageSlideState extends State<ConversationPageSlide>
     with SingleTickerProviderStateMixin {
   var controller;
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
