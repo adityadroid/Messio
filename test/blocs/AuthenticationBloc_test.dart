747b78dc19f198192612db1a98f57b1cf1c4b81b
Aditya Gurjar <aditya.gurjar@savaari.com>
2019-09-02 22:33:20 +0530

    Added tests fro AuthenticationBloc

--- test/blocs/AuthenticationBloc_test.dart
+++ test/blocs/AuthenticationBloc_test.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:messio/blocs/authentication/AuthenticationBloc.dart';
 import 'package:messio/blocs/authentication/AuthenticationEvent.dart';
